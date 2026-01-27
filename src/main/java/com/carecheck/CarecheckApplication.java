94860bcd42cf0ea7773a16a75b993973412129f6
ValdemarTaik <taik_red@mail.ru>
2026-01-27 16:26:41 +0300

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/carecheck/CarecheckApplication.java
@@ -0,0 +1,13 @@
+package com.carecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarecheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarecheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carecheck/CarecheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarecheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
